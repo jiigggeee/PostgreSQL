44440cafc58c5ffce98a380fb285117df60f5efb
Adrienne L <95113407+jiigggeee@users.noreply.github.com>
2023-04-28 15:37:09 -0400

    inserting data into customer tables

---
+++ SALES_DATABASE/inserting data into customer table.sql
@@ -0,0 +1,9 @@
+insert into customer (first_name, last_name, email, company, street, city, state, zipcode, sex, date_entered)
+values 
+('Tyler', 'Reynolds', 'rdawgpartay@gmail.com', 'Shoe Carnival', '487 Dickerson Rd', 'Denver', 'CO', '80203', 'M', '2023-01-16 15:15'),
+('Janet', 'Murrico', 'j.murrico@dsw.com', 'DSW', '108 Whishaw Ave', 'Denver', 'CO', '80203', 'F', '2022-12-16 08:35'),
+('Juan Ignacio', 'Perez', 'perez.ji@famousfootwear.com', 'Famous Footwear', '2080 Wiltshire Blvd', 'Los Angeles', 'CA', '90208', 'X', '2023-04-16 09:15'),
+('Tyler', 'Storey', 'goodvibesstosto@proton.me', 'Shoe King', '1895 Barrett Parkway', 'Kennesaw', 'GA', '30144', 'M', '2021-01-11 06:30'),
+('Amal', 'Yishma', 'amal.yishma@famousfootwear.com', 'Famous Footwear Outlet', '5018 North Hollowell Blvd #640', 'Bridgemill', 'CO', '80506', 'F', '2021-08-09 03:16'),
+('Patty', 'Waggoner', 'pattygurl@yahoo.com', '50 East Shoes', '915 Ridgewalk Pkwy', 'Woodstock', 'GA', '30188', 'F', '2022-07-13 13:18'),
+('Elle', 'Henry', 'ellehenry@rackroomshoes.com', 'Rack Room Shoes', '137 Woodstock Square Ave', 'Woodstock', 'GA', '30189', 'F', '2022-12-12 09:35');
