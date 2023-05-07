9d97d9c8305562b33a5ce75c4ec6607036530b79
Adrienne L <95113407+jiigggeee@users.noreply.github.com>
2023-05-07 01:20:35 -0400

    Inserting sales_person data
    Inserting sales_person data into sales_person table using SQL.

---
+++ SALES_DATABASE/inserting data into sales_person table.sql
@@ -0,0 +1,5 @@
+insert into sales_person (first_name, last_name, email, street, city, state, zip, phone, birth_date, sex, date_hired)
+values
+('Angela', 'Davis', 'angela.davis@shoeco.us', '1355 Henderson St', 'Rutherford', 'New Jersey', '20235', '112-595-7989', '1971-05-04', 'F','2005-09-12'),
+('Beryl', 'Cauley', 'beryl.cauley@shoeco.us', '3335 Speed Way', 'Daytona', 'Florida', '40512', '870-574-8812', '1965-02-06', 'X', '1999-12-22'),
+('Mary', 'Pasey', 'mary.pasey@shoeco.us', '1256 Ellery Avenue', 'Jefferson Heights', 'New York', '50212', '225-546-6456', '1980-05-09', 'F', '2012-11-12');
